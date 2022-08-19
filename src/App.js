003ad6c85d3dd2f1dc139ca6fc53969a0b7c2d9b
fabiodev30 <fabiodev30@gmail.com>
2022-08-19 15:30:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import About from './components/About';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Header />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+
+    </main>
   );
 }
 
