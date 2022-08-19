ced05c0c648731a1aa31e0287997b315f14f7001
fabiodev30 <fabiodev30@gmail.com>
2022-08-19 20:54:16 +0200

    add pb to card projects

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = (props) => {
   return (
     <a href={props.linksite}>
-    <div className="card" style={{width: '100%', height: '200px'}}>
+    <div className="card pb-1" style={{width: '100%', height: '200px'}}>
       <img src={props.image} className="img-fluid h-100" alt="..." />
     </div>
     </a>
