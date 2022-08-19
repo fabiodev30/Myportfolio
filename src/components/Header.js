78938df394be32c41e2a9c0add9f0f22154108e5
fabiodev30 <fabiodev30@gmail.com>
2022-08-19 15:51:28 +0200

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav className="navbar navbar-expand-lg bg-dark">
+      <div className="container p-3">
+        <a className="navbar-brand text-white" href="#">
+          FABIO ANGELI
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">
+                ABOUT
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">
+                PROJECTS
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white">SKILLS</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white">CONTACT</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
